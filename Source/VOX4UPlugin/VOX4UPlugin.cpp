90f9b021fba6a25f35480fb9137b001cd38baa78
mik14a <mik14a@gmail.com>
2016-04-10 02:52:36 +0900

    Override to 4.11.1 default

--- Source/VOX4UPlugin/VOX4UPlugin.cpp
+++
@@ -1,5 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "VOX4UPlugin.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VOX4UPlugin, "VOX4UPlugin" );
