aebb1796ce530322d5f37982111532bcf2be134b
mik14a <mik14a@gmail.com>
2016-04-11 04:21:19 +0900

    Add source for debug

---
+++ Source/VOX4UPlugin/VOX4UPlugin.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "VOX4UPlugin.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VOX4UPlugin, "VOX4UPlugin" );
---
+++ Source/VOX4UPlugin/VOX4UPluginWorldSettings.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "VOX4UPlugin.h"
+#include "VOX4UPluginWorldSettings.h"
+
+
+
+
