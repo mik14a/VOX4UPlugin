30aea11450c8fbc0d1dbe17adae5304dcabd67f1
mik14a <mik14a@gmail.com>
2020-03-29 02:28:52 +0900

    Update to 4.22.
    Change visual studio version.

--- GenerateProjectFiles.VS2017.cmd
+++
@@ -1,2 +0,0 @@
-set UnrealBuildTool="C:/Program Files/Epic Games/UE_4.21/Engine/Binaries/DotNET/UnrealBuildTool.exe"
-%UnrealBuildTool% -projectfiles -vs2017 -project="%~dp0VOX4UPlugin.uproject" -game -rocket
---
+++ GenerateProjectFiles.VS2019.cmd
@@ -0,0 +1,2 @@
+set UnrealBuildTool="C:/Program Files/Epic Games/UE_4.22/Engine/Binaries/DotNET/UnrealBuildTool.exe"
+%UnrealBuildTool% -projectfiles -vs2019 -project="%~dp0VOX4UPlugin.uproject" -game -rocket
