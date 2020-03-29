59c5d4d929b18b48fe91cf2f438e45a174222f0e
mik14a <mik14a@gmail.com>
2020-03-29 16:57:06 +0900

    Update to 4.23.

--- GenerateProjectFiles.VSCode.cmd
+++ GenerateProjectFiles.VSCode.cmd
@@ -1,2 +1,2 @@
-set UNREALBUILDTOOL="C:/Program Files/Epic Games/UE_4.22/Engine/Binaries/DotNET/UnrealBuildTool.exe"
+set UNREALBUILDTOOL="C:/Program Files/Epic Games/UE_4.23/Engine/Binaries/DotNET/UnrealBuildTool.exe"
 %UNREALBUILDTOOL% -projectfiles -vscode -project="%~dp0VOX4UPlugin.uproject" -game -rocket
