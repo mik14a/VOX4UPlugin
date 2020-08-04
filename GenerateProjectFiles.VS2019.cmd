947abdbd480834531917678c65518ca4b283facc
mik14a <mik14a@gmail.com>
2020-08-05 04:10:15 +0900

    Update to 4.25.
    Add VS2019 option, because the default compiler changed to 2017 when BuildPlugin.
    See BuildPlugin.ExecuteBuild in BuildPluginCommand.Automation.cs more details.

--- GenerateProjectFiles.VS2019.cmd
+++ GenerateProjectFiles.VS2019.cmd
@@ -1,2 +1,2 @@
-set UnrealBuildTool="C:/Program Files/Epic Games/UE_4.24/Engine/Binaries/DotNET/UnrealBuildTool.exe"
+set UnrealBuildTool="C:/Program Files/Epic Games/UE_4.25/Engine/Binaries/DotNET/UnrealBuildTool.exe"
 %UnrealBuildTool% -projectfiles -vs2019 -project="%~dp0VOX4UPlugin.uproject" -game -rocket
